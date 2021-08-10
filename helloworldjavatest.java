b6d7ac06e720e56226909fa9078a38721a91cd07
parakhi88 <88652399+parakhi88@users.noreply.github.com>
2021-08-10 21:50:18 +0530

    Update helloworldjavatest.java

--- helloworldjavatest.java
+++ helloworldjavatest.java
@@ -2,6 +2,6 @@ public class helloworldjavatest{
 
 	public static void main(String[] args){
 		
-		System.out.println("Hello World - polling test by ra");
+		System.out.println("Hello World - polling test by ravi");
 	}
 }
